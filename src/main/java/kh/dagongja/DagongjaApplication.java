44c2a8eeeccaddd3081ee1b72f2029e66b175313
acorn1066 <acorn1066@gmail.com>
2026-04-20 17:00:32 +0900

    Initial commit: Spring Boot project setup

---
+++ src/main/java/kh/dagongja/DagongjaApplication.java
@@ -0,0 +1,13 @@
+package kh.dagongja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DagongjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DagongjaApplication.class, args);
+	}
+
+}
