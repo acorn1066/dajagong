e6f421fdf88c0ccf219b056b1971dae3228d8618
acorn1066 <acorn1066@gmail.com>
2026-04-29 17:49:30 +0900

    chore: 어드민 페이지 기초 추가

--- src/main/java/kh/dajagong/admin/controller/AdminController.java
+++ src/main/java/kh/dajagong/admin/controller/AdminController.java
@@ -1,9 +1,10 @@
 package kh.dajagong.admin.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 
 @Controller
@@ -11,8 +12,45 @@ import lombok.RequiredArgsConstructor;
 public class AdminController {
 	
 	
-	@GetMapping("/admin/main")
-	public String main() {
-		return "/views/admin/main";
+	@GetMapping("/admin/member-management")
+	public String memberManagement(HttpServletRequest request, Model model) {
+		model.addAttribute("currentURI", request.getRequestURI());
+		return "/views/admin/member-management";
+	}
+	
+	@GetMapping("/admin/member-activity")
+	public String memberActivity(HttpServletRequest request, Model model) {
+		model.addAttribute("currentURI", request.getRequestURI());
+		return "/views/admin/member-management";
+	}
+	
+	@GetMapping("/admin/site-management-book")
+	public String siteManagementBook(HttpServletRequest request, Model model) {
+		model.addAttribute("currentURI", request.getRequestURI());
+		return "/views/admin/member-management";
+	}
+	
+	@GetMapping("/admin/site-management-QnA")
+	public String siteManagementQnA(HttpServletRequest request, Model model) {
+		model.addAttribute("currentURI", request.getRequestURI());
+		return "/views/admin/member-management";
+	}
+	
+	@GetMapping("/admin/log-mem")
+	public String logMember(HttpServletRequest request, Model model) {
+		model.addAttribute("currentURI", request.getRequestURI());
+		return "/views/admin/member-management";
+	}
+	
+	@GetMapping("/admin/log-book")
+	public String logBook(HttpServletRequest request, Model model) {
+		model.addAttribute("currentURI", request.getRequestURI());
+		return "/views/admin/member-management";
+	}
+	
+	@GetMapping("/admin/log-QnA")
+	public String logQnA(HttpServletRequest request, Model model) {
+		model.addAttribute("currentURI", request.getRequestURI());
+		return "/views/admin/member-management";
 	}
 }
