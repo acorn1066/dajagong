d060d1446ec022377d93e6ce4ba07cf8b06ede6c
acorn1066 <acorn1066@gmail.com>
2026-05-04 17:33:50 +0900

    feat: 로그인 추가

--- src/main/java/kh/dajagong/user/mapper/UserMapper.java
+++ src/main/java/kh/dajagong/user/mapper/UserMapper.java
@@ -2,7 +2,11 @@ package kh.dajagong.user.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import kh.dajagong.user.model.vo.User;
+
 @Mapper
 public interface UserMapper {
+
+	User login(User u);
 	
 }
---
+++ src/main/java/kh/dajagong/user/model/exception/UserException.java
@@ -0,0 +1,10 @@
+package kh.dajagong.user.model.exception;
+
+public class UserException extends RuntimeException{
+
+	public UserException() {}
+
+	public UserException(String msg) {
+		super(msg);
+	}
+}
