ab7702e93ab4c2d7a278fab5d0b12c6f07a88924
acorn1066 <acorn1066@gmail.com>
2026-05-04 17:33:50 +0900

    feat: 로그인 추가

--- src/main/java/kh/dajagong/user/controller/UserController.java
+++ src/main/java/kh/dajagong/user/controller/UserController.java
@@ -1,15 +1,27 @@
 package kh.dajagong.user.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
+import kh.dajagong.user.model.exception.UserException;
+import kh.dajagong.user.model.vo.User;
 import kh.dajagong.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
+@SessionAttributes("loginUser")
 public class UserController {
-	private UserService uService;
+	private final UserService uService;
+	
+	@GetMapping("/")
+	public String index() {
+	    return "index";
+	}
+	
 	
 	@GetMapping("login")
 	public String login() {
@@ -21,4 +33,18 @@ public class UserController {
 		return "views/user/enroll.html";
 	}
 	 
+	@PostMapping("/signIn")
+	public String login(User u, Model model) {
+		User loginUser = uService.login(u);
+		System.out.println(loginUser);
+		if(loginUser != null) {
+			model.addAttribute("loginUser", loginUser);
+			return "redirect:/";
+		}else {
+			throw new UserException("로그인을 실패하였습니다.");
+		}
+	}
+	
+	
+	
 }
