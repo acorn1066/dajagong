09bfe0d8646933f4d328bd6d998eb2261c054cb7
acorn1066 <acorn1066@gmail.com>
2026-05-06 14:39:24 +0900

    chore: 인덱스 페이지 책 목록 기능

--- src/main/java/kh/dajagong/book/review/service/BookReviewService.java
+++ src/main/java/kh/dajagong/book/review/service/BookReviewService.java
@@ -1,5 +1,20 @@
 package kh.dajagong.book.review.service;
 
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Service;
+
+import kh.dajagong.book.review.mapper.BookReviewMapper;
+import kh.dajagong.book.review.model.vo.Book;
+import lombok.RequiredArgsConstructor;
+@Service
+@RequiredArgsConstructor
 public class BookReviewService {
+	
+	private final BookReviewMapper mapper;
+
+	public ArrayList<Book> selectTop() {
+		return  mapper.selectTop();
+	}
 
 }
