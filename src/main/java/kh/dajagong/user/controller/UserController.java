9cde25db52a6e81d725cf6d06ba0f880a590251c
EunhoKim <ehkime0423@gmail.com>
2026-05-06 17:17:31 +0900

    1. 도서사진 - 151~160.jpg 2. 비밀번호 암호화 과정 완. DB쪽 user table pwd도 수정 완

--- src/main/java/kh/dajagong/user/controller/UserController.java
+++ src/main/java/kh/dajagong/user/controller/UserController.java
@@ -2,10 +2,13 @@ package kh.dajagong.user.controller;
 
 import java.util.ArrayList;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import kh.dajagong.book.review.model.vo.Book;
@@ -20,7 +23,8 @@ import lombok.RequiredArgsConstructor;
 @SessionAttributes("loginUser")
 public class UserController {
 	private final UserService uService;
-	private final BookReviewService bService; 
+	private final BookReviewService bService;
+	private final BCryptPasswordEncoder bcrypt;
 	
 	@GetMapping("/")
 	public String index(Model model) {
@@ -29,28 +33,48 @@ public class UserController {
 		return "index";
 	}
 	
-	
+	// 로그인 페이지 이동
 	@GetMapping("login")
 	public String login() {
 		return "views/user/login.html";
 	}
 	
-	@GetMapping("enroll")
-	public String enroll() {
-		return "views/user/enroll.html";
-	}
-	 
+	// 로그인 완료 후 메인으로
 	@PostMapping("/signIn")
 	public String login(User u, Model model) {
 		User loginUser = uService.login(u);
-		System.out.println(loginUser);
-		if(loginUser != null) {
+		if(loginUser != null && bcrypt.matches(u.getPwd(), loginUser.getPwd())) {
 			model.addAttribute("loginUser", loginUser);
 			return "redirect:/";
 		}else {
 			throw new UserException("로그인을 실패하였습니다.");
 		}
 	}
+	
+	// 회원가입 페이지로 이동
+	@GetMapping("enroll")
+	public String enroll() {
+		return "views/user/enroll.html";
+	}
+	
+	// 회원가입 완료 후 메인으로
+	@PostMapping("/")
+	public String enroll(@ModelAttribute User u, @RequestParam("id") String id) {
+
+		if(!id.trim().equals("")) {
+			u.setUserId(id);
+		}
+		
+		u.setPwd(bcrypt.encode(u.getPwd()));
+		
+		int result = uService.insertUser(u);
+		if(result > 0) {
+			return "redirect:/";
+		} else {
+			throw new UserException("회원가입을 실패하였습니다.");
+		}
+	}
+	 
 	
 	
 	
