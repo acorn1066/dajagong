59b48c8aa3998861ff0cccb47b0319f9e411f9fd
suyeol2000 <suyeolhan874@gmail.com>
2026-05-07 14:42:06 +0900

    아이고

--- src/main/java/kh/dajagong/qa/controller/QaController.java
+++ src/main/java/kh/dajagong/qa/controller/QaController.java
@@ -1,25 +1,54 @@
 package kh.dajagong.qa.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import kh.dajagong.common.PageInfo;
+import kh.dajagong.common.Pagination;
+import kh.dajagong.qa.model.vo.Question;
+import kh.dajagong.qa.service.QaService;
 import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/qa") // 중복되는 /qa 경로를 위로 뺐습니다.
 public class QaController {
+    
+    private final QaService qService;
 
-    @GetMapping("/qa/list")
-    public String qalist() {
-        return "/views/qa/qalist"; 
+    @GetMapping("/list")
+    public ModelAndView qalist(@RequestParam(value="page", defaultValue="1") int page, 
+                               ModelAndView mv) {
+        
+        // 1. DB에서 총 개수 조회
+        int listCount = qService.getListCount();
+        
+        // 2. 페이징 계산 (강의 표준 방식)
+        PageInfo pi = Pagination.getPageInfo(page, listCount, 10);
+        
+        // 3. 목록 조회
+        ArrayList<Question> list = qService.selectQaList(pi);
+        
+        // 4. 데이터 전달
+        mv.addObject("list", list);
+        mv.addObject("pi", pi);
+        mv.setViewName("views/qa/qalist"); 
+        
+        return mv;
     }
 
-    @GetMapping("/qa/Qwriter")
+    @GetMapping("/Qwriter")
     public String Qwriter() {
-        return "/views/qa/Qwriter";
+        return "views/qa/Qwriter"; // 앞의 슬래시(/)는 설정에 따라 생략 가능합니다.
     }
 
-    @GetMapping("/qa/Wwriter")
+    @GetMapping("/Wwriter")
     public String Wwriter() {
-        return "/views/qa/Wwriter";
+        return "views/qa/Wwriter";
     }
 }
