d09da491a800c5f5fd845ddc58dbf8b0eabf04e9
EunhoKim <ehkime0423@gmail.com>
2026-05-08 10:23:17 +0900

    1. userController에 있던 bookReviewController 관련 메소드 이전했습니다. 2. 로그아웃 쪽 에러가 있어서 코드 전면 수정했습니다. 정상 작동 합니다. 3. 로그아웃 관련 수정 중 관련된 html들(enroll, navbar, login) 일부 수정

--- src/main/java/kh/dajagong/user/controller/UserController.java
+++ src/main/java/kh/dajagong/user/controller/UserController.java
@@ -1,19 +1,13 @@
 package kh.dajagong.user.controller;
 
-import java.util.ArrayList;
-
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.SessionAttributes;
 
 import jakarta.servlet.http.HttpSession;
-import kh.dajagong.book.review.model.vo.Book;
-import kh.dajagong.book.review.service.BookReviewService;
 import kh.dajagong.common.exception.UserException;
 import kh.dajagong.user.model.vo.User;
 import kh.dajagong.user.service.UserService;
@@ -21,31 +15,23 @@ import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
-@SessionAttributes("loginUser")
 public class UserController {
 	private final UserService uService;
-	private final BookReviewService bService;
 	private final BCryptPasswordEncoder bcrypt;
 	
-	@GetMapping("/")
-	public String index(Model model) {
-		ArrayList<Book> bookList = bService.selectTop();
-		model.addAttribute("bookList",bookList);
-		return "index";
-	}
 	
 	// 로그인 페이지 이동
 	@GetMapping("login")
 	public String login() {
-		return "views/user/login.html";
+		return "views/user/login";
 	}
 	
 	// 로그인 완료 후 메인으로
 	@PostMapping("/signIn")
-	public String login(User u, Model model) {
+	public String login(User u, HttpSession session) {
 		User loginUser = uService.login(u);
 		if(loginUser != null && bcrypt.matches(u.getPwd(), loginUser.getPwd())) {
-			model.addAttribute("loginUser", loginUser);
+			session.setAttribute("loginUser", loginUser);
 			return "redirect:/";
 		}else {
 			throw new UserException("로그인을 실패하였습니다.");
@@ -55,11 +41,11 @@ public class UserController {
 	// 회원가입 페이지로 이동
 	@GetMapping("enroll")
 	public String enroll() {
-		return "views/user/enroll.html";
+		return "views/user/enroll";
 	}
 	
 	// 회원가입 완료 후 메인으로
-	@PostMapping("/")
+	@PostMapping("/enroll")
 	public String enroll(@ModelAttribute User u, @RequestParam("id") String id) {
 
 		if(!id.trim().equals("")) {
@@ -77,12 +63,17 @@ public class UserController {
 	}
 	
 	// 로그아웃 후 메인으로
-	@GetMapping("logout")
+	@GetMapping("log-out")
 	public String logout(HttpSession session) {
-		session.invalidate();
-		return "redirect:/";
+	    session.invalidate();
+	    return "redirect:/";
 	}
 	 
+	// MyPage 페이지로
+	@GetMapping("myPage")
+	public String myPage() {
+		return "views/user/myPage";
+	}
 	
 	
 	
